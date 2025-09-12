c9f4f5846d863c0872f431c48f2d3ea03263ec48
rémi barjavel <remi.barjavel@epseed.org>
2025-09-12 17:52:54 +0200

    Cookies from file.

--- src/video_processing.rs
+++ src/video_processing.rs
@@ -74,8 +74,8 @@ impl VideoProcessor {
 
         let mut download_cmd = AsyncCommand::new("yt-dlp");
         download_cmd.args([
-            "--cookies-from-browser",
-            "firefox",
+            "--cookies",  // Use --cookies instead of --cookies-from-browser
+            "./firefox_cookies.txt", // Path to your cookies file
             "--format",
             "bv*[ext=mp4][height<=720]+ba[ext=m4a]/bv*[ext=mp4][height<=720]/b[ext=mp4]",
             "--output",
